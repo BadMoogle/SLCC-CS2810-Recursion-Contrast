cf7fad5aa1eb4c82de84452dc00d85b811aec61b
Daniel Harper <daniel.c.harper@live.com>
2021-08-28 12:30:35 -0600

    Initial Commit

---
+++ build/CMakeFiles/recursioncontrast.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/recursioncontrast.dir/main.cpp.o"
+  "recursioncontrast.pdb"
+  "recursioncontrast"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/recursioncontrast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
